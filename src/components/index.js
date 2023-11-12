a4ccbdf679b57d8d4358c08ebb704393acaecaab
shashwath <shashukarkera01@gmail.com>
2023-11-12 23:49:53 +0530

    chore: default exports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as Viewbar } from './Viewbar';
+export { default as Table} from './Table';
+export { default as Monitor} from './Monitor';
+export {default as SearchField} from './SearchField';
+export {default as TableRow} from './TableRow';
+export {default as Dropdown} from './Dropdown';
+export {default as CloseAccountModal} from './CloseAccountModal';
+export {default as OnProgress} from './OnProgress';
