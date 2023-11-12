7cda3d7eabe337bedb914baa08b3e873a6815029
shashwath <shashukarkera01@gmail.com>
2023-11-12 23:48:56 +0530

    feat: create a page that handels all un handeled paths

---
+++ src/pages/PageNotFound/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+function PageNotFound() {
+    return (
+        <div className="pageNotFoundContainer">
+            <p>404 Error. Page not found</p>
+        </div>
+    );
+}
+
+export default PageNotFound;
