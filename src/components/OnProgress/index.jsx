f79550aec4b2529e02eab4102060955643f3ef00
shashwath <shashukarkera01@gmail.com>
2023-11-12 23:53:15 +0530

    chore: add a page informing user reuested service is not available at the moment

---
+++ src/components/OnProgress/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.css';
+
+function OnProgress() {
+    return (
+        <div className="OnProgress-container">
+            <p className="OnProgress-text">Working on this section</p><br/>
+            <p className="OnProgress-text">Please redirect to /dashboard</p>
+        </div>
+    );
+}
+
+export default OnProgress;
